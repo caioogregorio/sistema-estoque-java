fb1eba4b907df99f0a43fdd81e27edc960b40967
Caio dos Santos Gregório da Rocha <caio.grego.roc@gmail.com>
2026-05-27 23:56:41 -0300

    Cria a classe produto

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -2,4 +2,52 @@ package entities;
 
 public class Produto {
 
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public Produto() {
+		
+	}
+
+	public Produto(String nome, Double preco, Integer quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	@Override
+	public String toString() {
+	    return "Nome: "
+	            + nome
+	            + "\nPreço: R$ "
+	            + preco
+	            + "\nQuantidade: "
+	            + quantidade;
+	}
+	
 }
