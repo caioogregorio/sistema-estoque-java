0f418daa1b6ca5032f8c035d5ea35f9e5171d5a1
Caio dos Santos Gregório da Rocha <caio.grego.roc@gmail.com>
2026-05-28 00:56:23 -0300

    Implementa cadastro e listagem de produtos

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,27 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Estoque;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Deseja cadastrar um produto?(1-sim|2-não): ");
+		int opcao = sc.nextInt();
+		
+		if(opcao == 1) {
+			System.out.println("Insira os dados do produto que voce quer cadastrar");
+			Estoque p = new Estoque();
+			p.adicionarProduto();
+			p.listarProdutos();
+		}
+		else {
+			System.out.println("A lista esta vazia");
+		}
 
 	}
 
--- src/entities/Estoque.java
+++ src/entities/Estoque.java
@@ -1,5 +1,47 @@
 package entities;
 
-public class Estoque {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-}
+public class Estoque {
+	
+	private ArrayList<Produto> produtos = new ArrayList<>();
+	
+	Scanner sc = new Scanner(System.in);
+	Produto p = new Produto();
+	
+	public void adicionarProduto() {
+		
+			int opcao;
+			do {
+				sc.nextLine();
+				System.out.println("Nome: ");
+				String nome = sc.nextLine();
+				System.out.println("Preço: ");
+				double preco = sc.nextDouble();
+				System.out.println("Quantidade: ");
+				int quantidade = sc.nextInt();
+				
+				Produto p = new Produto(nome, preco, quantidade);
+				
+				produtos.add(p);
+				
+				System.out.println("Deseja continuar cadastrando(Digite 1 se quiser sair)?");
+				opcao = sc.nextInt();
+			}while(opcao != 1);
+		
+	}
+	
+	public void listarProdutos() {
+			int quantidade = 1;
+			for (Produto p : produtos) {
+				System.out.println("Produto número " + quantidade);
+				System.out.println(p);
+				quantidade += 1;
+			}
+	}
+			
+			
+		}
+
+
